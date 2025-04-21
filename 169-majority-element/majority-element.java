5232fc9bc05eb92e2ebb0f5cdbca14ee57fe96ba
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-04-21 13:09:08 +0530

    Time: 1 ms (99.88%) | Memory: 53.1 MB (60.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,18 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    Map<Integer, Integer> elementToTimesMap = new HashMap<>();
-    for (int num : nums) {
-      if (!elementToTimesMap.containsKey(num)) {
-        elementToTimesMap.put(num, 1);
-      } else {
-        elementToTimesMap.put(num, elementToTimesMap.get(num) + 1);
-      }
+        // Space -> O(n) and Time -> O(1)
+//     Map<Integer, Integer> elementToTimesMap = new HashMap<>();
+//     for (int num : nums) {
+//       if (!elementToTimesMap.containsKey(num)) {
+//         elementToTimesMap.put(num, 1);
+//       } else {
+//         elementToTimesMap.put(num, elementToTimesMap.get(num) + 1);
+//       }
+//     }
+//     for (Map.Entry<Integer, Integer> entry : elementToTimesMap.entrySet()) {
+//       if (entry.getValue() > nums.length / 2) {
+//         return entry.getKey();
+//       }
+//     }
+//     return 0;
+
+        // Space -> O(1) and Time -> O(1)
+
+    int count = 0;
+    int candidate = 0;
+
+    for(int num: nums){
+        if(count == 0)
+            candidate = num;
+
+        if(num == candidate) count++;
+        else count--;
     }
-    for (Map.Entry<Integer, Integer> entry : elementToTimesMap.entrySet()) {
-      if (entry.getValue() > nums.length / 2) {
-        return entry.getKey();
-      }
-    }
-    return 0;
+    return candidate;
   }
 }
