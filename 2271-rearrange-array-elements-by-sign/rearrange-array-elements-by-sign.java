802f40a06ca2a9f2ad597fe141c4154b626e5c30
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-04-29 15:38:30 +0530

    Time: 6 ms (27.09%) | Memory: 86.2 MB (5.22%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,26 +1,6 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-    //     int positive=0, negative=1, normal=0;
-    //     if(nums.length < 2)return nums;
-    //     while(normal < nums.length){
-    //         if(nums[normal] >= 0 && normal != positive && positive < nums.length && nums[positive] < 0){
-    //             swap(nums, normal, positive);
-    //             positive += 2; 
-    //         } else if (positive < nums.length && nums[positive] >= 0) positive+=2;
-    //         else if(nums[normal] < 0 && normal != negative && negative < nums.length && nums[negative] >= 0){
-    //             swap(nums, normal, negative);
-    //             negative += 2;
-    //         } else if(negative < nums.length && nums[negative] < 0 )negative += 2;
-    //         normal++;
-    //     }
-    //     return nums;
-    // }
-
-    // void swap(int[] nums, int start, int end){
-    //     int temp = nums[start];
-    //     nums[start] = nums[end];
-    //     nums[end] = temp;
-    // }
+        // Time -> O(n) and Space -> O(n)
     int n = nums.length;
     int[] positive = new int[n/2];
     int[] negative = new int[n/2];
