f78022fae072e82a3d57f99ad18444b96dc62d18
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-03 12:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (41.53%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,18 +1,48 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int[][] rotatedMatrix = new int[n][n];
+        // int[][] rotatedMatrix = new int[n][n];
+        // Brute force - > time -> O(n^2) & space -> O(n^2)
+        // for(int row=0; row<n; row++){
+        //     for(int col=0; col<n; col++){
+        //         rotatedMatrix[col][n-row-1] = matrix[row][col];
+        //     }
+        // }
 
+        // for(int row=0; row<n; row++){
+        //     for(int col=0; col<n; col++){
+        //         matrix[row][col] = rotatedMatrix[row][col];
+        //     }
+        // }
+
+        // Optimal -> Transpose the matrix and reverse each row.
+        // To Transpose -> Swap all the non diagonal elements of the matrix.
+        // Time => O(n^2) & Space => O(1)
         for(int row=0; row<n; row++){
-            for(int col=0; col<n; col++){
-                rotatedMatrix[col][n-row-1] = matrix[row][col];
+            for(int col=row; col<n; col++){
+                if(row != col){
+                int temp = matrix[row][col];
+                    matrix[row][col] = matrix[col][row];
+                    matrix[col][row] = temp;
+                }
             }
         }
 
         for(int row=0; row<n; row++){
-            for(int col=0; col<n; col++){
-                matrix[row][col] = rotatedMatrix[row][col];
-            }
+            reverse(matrix[row]);
+        }
+
+    }
+
+    void reverse(int[] arr){
+        int i=0, j=arr.length-1;
+
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
         }
     }
 }
