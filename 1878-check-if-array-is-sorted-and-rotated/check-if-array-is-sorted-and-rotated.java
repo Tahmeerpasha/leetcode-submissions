9f82f082301a35be56013703f823cc6c1e270107
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-04 15:15:32 +0530

    Update check-if-array-is-sorted-and-rotated.java

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean check(int[] nums) {
-        int breakPoint = 0;
         int n = nums.length;
-
-        if(nums[n-1] > nums[0]) breakPoint++;
-
-        for(int i=0; i<n-1; i++){
-            if(nums[i] > nums[i+1])
-                breakPoint++;
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) count++;
         }
-       return !(breakPoint>1);
+        if (nums[0] < nums[n - 1]) count++;
+        return count <= 1;
     }
 }
