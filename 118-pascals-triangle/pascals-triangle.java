0c44e8fec38c8bb8d92cae0d87a68419f6d28362
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-06 16:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,14 +1,32 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=1; i<=numRows; i++){
-        List<Integer> answer = new ArrayList<Integer>();
-            for(int j=1; j<=i; j++){
-                answer.add(nCr(i-1,j-1));
-            }
-            list.add(answer);
+        List<List<Integer>> result = new ArrayList<>();
+        // Time -> O(n*n*r) -> O(n^3) & Space -> O(1)
+        // for(int i=1; i<=numRows; i++){
+        // List<Integer> answer = new ArrayList<Integer>();
+        //     for(int j=1; j<=i; j++){
+        //         answer.add(nCr(i-1,j-1));
+        //     }
+        //     result.add(answer);
+        // }
+
+        // Time -> O(n^2) & Space -> O(1)
+        for(int row=1; row<=numRows; row++){
+            result.add(generateRow(row));
         }
-        return list;
+        return result;
+    }
+
+    List<Integer> generateRow(int row){
+        List<Integer> answer = new ArrayList<Integer>();
+        int ans = 1;
+        answer.add(1);
+        for(int col=1; col<row; col++){
+            ans *= row-col;
+            ans /= col;
+            answer.add(ans);
+        }
+        return answer;
     }
 
     int nCr(int n, int r){
