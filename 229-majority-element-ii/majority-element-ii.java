ba22f0e34b1ce2a186a98a73d01e42677e0d7975
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-06 17:22:26 +0530

    Time: 3 ms (71.95%) | Memory: 50.4 MB (5.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,15 +1,50 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> elementCountMap = new HashMap<>();
         List<Integer> resultantArray = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i],0)+1);
+
+        // Time -> O(n) & Space -> O(n)
+        // Map<Integer, Integer> elementCountMap = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++){
+        //     elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i],0)+1);
+        // }
+        // elementCountMap.forEach((key, value) -> {
+        // if(value > nums.length/3){
+        //     resultantArray.add(key);
+        // }
+        // });
+
+        // Optimal Approach -> Space = O(1) & Time = O(n)
+        int count1 = 0, count2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 == 0 && nums[i] != ele2) {
+                count1 = 1;
+                ele1 = nums[i];
+            } else if (count2 == 0 && nums[i] != ele1) {
+                count2 = 1;
+                ele2 = nums[i];
+            } else if (ele1 == nums[i])
+                count1++;
+            else if (ele2 == nums[i])
+                count2++;
+            else {
+                count1--;
+                count2--;
+            }
         }
-        elementCountMap.forEach((key, value) -> {
-        if(value > nums.length/3){
-            resultantArray.add(key);
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == ele1)
+                count1++;
+            if (nums[i] == ele2)
+                count2++;
         }
-        });
+        if (nums.length / 3 < count1)
+            resultantArray.add(ele1);
+        if (nums.length / 3 < count2)
+            resultantArray.add(ele2);
         return resultantArray;
     }
 }
