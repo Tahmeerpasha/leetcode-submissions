fefbe4a956e5443c52ab869e3c75f1ea99b662ab
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-08 20:42:32 +0530

    Time: 1 ms (99.60%) | Memory: 57.2 MB (43.59%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,11 +1,62 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=0, max=Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++){
-            if(sum < 0)sum = 0;
-            sum += nums[i];
-            max = Math.max(max,sum);
-        }
-        return max;
+    // ArrayList<Integer> answers = new ArrayList<>();
+    int n = nums.length;
+    if(n <= 1)return nums[0];
+
+    // // O(n^3) -> time and space -> O(n^3)
+    // for(int i=0; i<n ; i++){
+    //   for(int j=i; j<n; j++){
+    //     int sum = 0;
+    //     for(int k=i; k<=j; k++)
+    //       sum += nums[k];
+    //     answers.add(sum);
+    //   }
+    // }
+    // answers.sort(null);
+    // return !answers.isEmpty() ? answers.get(answers.size()-1) : 0;
+
+    // // O(n^2) -> time and space -> O(n^2)
+    // for(int i=0; i<n ; i++){
+    // int sum = 0;
+    //   for(int j=i; j<n; j++){
+    //       sum += nums[j];
+    //     answers.add(sum);
+    //   }
+    // }
+    // answers.sort(null);
+    // return !answers.isEmpty() ? answers.get(answers.size()-1) : 0;
+
+    // O(n^2) -> time and space -> O(1)
+    // int max = Integer.MIN_VALUE;
+    // for(int i=0; i<n ; i++){
+    // int sum = 0;
+    //   for(int j=i; j<n; j++){
+    //       sum += nums[j];
+    //       max = Math.max(max,sum);
+    //   }
+    // }
+    // return max;
+
+    // O(n^2) -> time and space -> O(1)
+    // int max = Integer.MIN_VALUE;
+    // for(int i=0; i<n ; i++){
+    // int sum = 0;
+    //   for(int j=i; j<n; j++){
+    //       sum += nums[j];
+    //       max = Math.max(max,sum);
+    //   }
+    // }
+    // return max;
+
+    // O(n) -> time and space -> O(1)
+    int maximum = Integer.MIN_VALUE;
+    int sum = 0;
+    for(int index=0; index<n; index++){
+        if(sum<0) sum=0;
+        sum += nums[index];
+        maximum = Math.max(maximum, sum);
     }
+    return maximum;
+  }
 }
