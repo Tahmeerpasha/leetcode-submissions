b4c79715f349934bd9d8747397011fbd0ffbde16
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-10 16:39:38 +0530

    Time: 18 ms (60.24%) | Memory: 44.4 MB (25.31%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -20,24 +20,49 @@ class Solution {
         //     }
         // }
 
+        // Better = Time -> O(n^3) && Space -> O(1)
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         Set<Long> set = new HashSet<>();
+        //         for(int k=j+1; k<n; k++){
+        //             long sum = nums[i] + nums[j];
+        //             sum += nums[k];
+        //             long fourth = target - sum;
+        //             if(set.contains(fourth)){
+        //                 List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], (int)fourth);
+        //                 temp.sort(null);
+        //                 result.add(temp);
+        //             }
+        //             set.add((long)nums[k]);
+        //         }
+        //     }
+        // }
+
+        Arrays.sort(nums);
         for(int i=0; i<n; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j=i+1; j<n; j++){
-                Set<Long> set = new HashSet<>();
-                for(int k=j+1; k<n; k++){
-                    long sum = nums[i] + nums[j];
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k=j+1, l=n-1;
+                while(k<l){
+                    long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long fourth = target - sum;
-                    if(set.contains(fourth)){
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], (int)fourth);
+                    sum += nums[l];
+                    if(sum < target)k++;
+                    else if(sum > target)l--;
+                    else{
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
                         temp.sort(null);
                         result.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k] == nums[k-1])k++;
+                        while(k<l && nums[l] == nums[l+1])l--;
                     }
-                    set.add((long)nums[k]);
                 }
             }
         }
-
-
         return new ArrayList<>(result);
     }
 }
