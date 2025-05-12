758d0b24f2d2e92727a11dbbdc92e59472f08ac8
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-12 20:34:30 +0530

    Time: 8 ms (86.73%) | Memory: 46.3 MB (91.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,14 +3,15 @@ class Solution {
         int n = intervals.length;
         List<int[]> answer = new ArrayList<>();
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        // Brute force -> O(2n)
+        // Brute force -> Time = O(2n) && Space = O(n)
         // for(int i=0; i<n; i++){
+        // select an interval:
         //     int start = intervals[i][0];
         //     int end = intervals[i][1];
-
+        //Skip all the merged intervals:
         //     if(!answer.isEmpty() && end <= answer.get(answer.size()-1)[1])
         //         continue;
-            
+        //check the rest of the intervals:
         //     for(int j=i+1; j<n; j++){
         //         if(intervals[j][0] <= end)
         //             end = Math.max(end, intervals[j][1]);
@@ -19,10 +20,15 @@ class Solution {
         //     answer.add(new int[]{start,end});
         // }
 
+        // Optimal -> Time = O(n) && Space -> O(n)
         for(int i=0; i<n; i++){
+            // if the current interval does not
+            // lie in the last interval:
             if(answer.isEmpty() || intervals[i][0] > answer.get(answer.size()-1)[1])
                 answer.add(new int[]{intervals[i][0], intervals[i][1]});
             else
+            // if the current interval
+            // lies in the last interval:
                 answer.get(answer.size()-1)[1] = Math.max(answer.get(answer.size()-1)[1], intervals[i][1]);
         }
 
