c9f6c1b392bae31c80bde6a007fbb5a09eaf369e
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-13 12:45:18 +0530

    Time: 1 ms (28.51%) | Memory: 42.1 MB (77.28%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,31 +1,37 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         // Brute -> Time = O(m+n) && Space -> O(m+n)
-        int[] result = new int[m+n];
-        int first=0, second=0, third=0;
-        while(first < m && second < n){
-            if(nums1[first] < nums2[second]){
-                result[third++] = nums1[first];
-                first++;
-            }else if(nums2[second] < nums1[first]){
-                result[third++] = nums2[second];
-                second++;
-            }else{
-                result[third++] = nums1[first++];
-                result[third++] = nums2[second++];
-            }
-        }
+        // int[] result = new int[m+n];
+        // int first=0, second=0, third=0;
+        // while(first < m && second < n){
+        //     if(nums1[first] < nums2[second]){
+        //         result[third++] = nums1[first];
+        //         first++;
+        //     }else if(nums2[second] < nums1[first]){
+        //         result[third++] = nums2[second];
+        //         second++;
+        //     }else{
+        //         result[third++] = nums1[first++];
+        //         result[third++] = nums2[second++];
+        //     }
+        // }
 
-        while(first < m){
-            result[third++] = nums1[first++];
-        }
+        // while(first < m){
+        //     result[third++] = nums1[first++];
+        // }
 
-        while(second < n){
-            result[third++] = nums2[second++];
-        }
+        // while(second < n){
+        //     result[third++] = nums2[second++];
+        // }
 
-        for(int i=0; i<m+n; i++){
-            nums1[i] = result[i];
+        // for(int i=0; i<m+n; i++){
+        //     nums1[i] = result[i];
+        // }
+
+        // Optimal -> Time = O(nlogn) && Space = O(1)
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[i-m];
         }
+        Arrays.sort(nums1);
     }
 }
