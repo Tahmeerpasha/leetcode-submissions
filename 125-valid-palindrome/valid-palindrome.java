fc880b0d1a61e1003a3b76c8df20c7c7bdc85643
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-15 20:46:19 +0530

    Time: 15 ms (21.89%) | Memory: 45.1 MB (23.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,25 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-       return findPalindrome(s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray(),0);
-    }
+    // public boolean isPalindrome(String s) {
+    //    return findPalindrome(s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray(),0);
+    // }
 
-    boolean findPalindrome(char[] s, int i){
-        if(i>=s.length/2)return true;
-        int n = s.length-i-1;
-        if(s[i] != s[n])return false;
-        else return findPalindrome(s, i+1);
+    // boolean findPalindrome(char[] s, int i){
+    //     if(i>=s.length/2)return true;
+    //     int n = s.length-i-1;
+    //     if(s[i] != s[n])return false;
+    //     else return findPalindrome(s, i+1);
+    // }
+
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
