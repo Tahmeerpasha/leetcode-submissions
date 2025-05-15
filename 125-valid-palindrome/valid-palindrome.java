fbe4c5aeb140c1287e117606d0f88b22c445922d
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-15 20:49:29 +0530

    Time: 3 ms (73.23%) | Memory: 43.2 MB (69.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,4 @@
 class Solution {
-    // public boolean isPalindrome(String s) {
-    //    return findPalindrome(s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray(),0);
-    // }
-
-    // boolean findPalindrome(char[] s, int i){
-    //     if(i>=s.length/2)return true;
-    //     int n = s.length-i-1;
-    //     if(s[i] != s[n])return false;
-    //     else return findPalindrome(s, i+1);
-    // }
-
     public boolean isPalindrome(String s) {
         int left = 0, right = s.length() - 1;
 
@@ -32,7 +21,6 @@ class Solution {
             left++;
             right--;
         }
-
         return true;
     }
 
