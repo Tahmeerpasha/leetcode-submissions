ea383c45c94fc8ba0b955eb099e20bd6461286d7
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-18 14:43:56 +0530

    Time: 18 ms (88.51%) | Memory: 56.5 MB (95.28%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,37 +1,17 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int count=0, maxLength=0, lastSmaller=Integer.MIN_VALUE;
-        // O(nlogn)
-        // Arrays.sort(nums);
-        // for(int i=0; i<nums.length; i++){
-        //     if(nums[i]-1 == lastSmaller){
-        //         count++;
-        //         lastSmaller = nums[i];
-        //     }else if(nums[i] != lastSmaller){ // if it's equal do nothing
-        //         count = 1;
-        //         lastSmaller = nums[i];
-        //     }
-        //     maxLength = Math.max(maxLength, count);
-        // }
-        // return maxLength;
-
-        // O(n)
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
-        }
-        
-        for(int it: set){
-            if(!set.contains(it-1)){
-                int cnt = 1;
-                int x = it;
-                while(set.contains(x+1)){
-                    x += 1;
-                    cnt += 1;
-                }
-                maxLength = Math.max(maxLength, cnt);
+        Arrays.sort(nums);
+        if(nums.length<1)return 0;
+        int count=1, max=1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i+1]-nums[i] == 1){
+                count++;
+                max = Math.max(max, count);
             }
+            else if(nums[i+1]-nums[i] == 0)continue;
+            else count=1;
         }
-        return maxLength;
+        return max;
+
     }
 }
