f150c24b7077e701854451e239b9c9ca58e31fb6
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-23 07:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (48.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int low = 0, high = n-1;
         int first=-1, last=-1;
-        // Not optimal - Using loops 
+        // Not optimal - Using loops. Time -> more than O(nlogn)
         // while(low <= high){
         //     int mid = (low+high)/2;
         //     if(nums[mid] == target){
@@ -20,11 +20,34 @@ class Solution {
         // }
         // return new int[]{first, last};
 
-        // Using upper and lower bounds
-        int lower = lowerBound(nums, nums.length, target);
-        if(lower == nums.length || nums[lower] != target)return new int[]{-1,-1};
-        int upper = upperBound(nums, nums.length, target);
-        return new int[]{lower, upper-1};
+        // Using upper and lower bounds => Time -> O(nlogn)
+        // int lower = lowerBound(nums, nums.length, target);
+        // if(lower == nums.length || nums[lower] != target)return new int[]{-1,-1};
+        // int upper = upperBound(nums, nums.length, target);
+        // return new int[]{lower, upper-1};
+
+        // Simple binary search for first and last
+        // For first occurrence
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                first = mid;
+                high = mid-1;
+            }else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        low = 0;
+        high = n-1;
+        // For last occurrence
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                last = mid;
+                low = mid+1;
+            }else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return new int[]{first, last};
     }
 
     public int lowerBound(int []arr, int n, int x) {
