449866c598520be749af673c8978e02d2e32a17a
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-24 14:31:04 +0530

    Time: 1 ms (86.49%) | Memory: 40.7 MB (93.09%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,10 +1,20 @@
 class Solution {
     public int mySqrt(int x) {
-        // Brute => Time -> O(n)
         int ans = 0;
-        for(int i=1; i<=x; i++){
-            if((long) i*i <= x)ans = i;
-            else break;
+        // Brute => Time -> O(n)
+        // for(int i=1; i<=x; i++){
+        //     if((long) i*i <= x)ans = i;
+        //     else break;
+        // }
+        
+        // Optimal - Using binary search
+        int low = 1, high = x;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if((long)mid * mid <= x){
+                ans = mid;
+                low = mid+1;
+            }else high = mid-1;
         }
         return ans;
     }
