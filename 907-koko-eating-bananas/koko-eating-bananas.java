dacc3e3d4b85fc20b77f62e3fb4f60f7ddcf5064
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-29 20:09:25 +0530

    Time: 25 ms (41.54%) | Memory: 45.1 MB (61.09%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,25 +1,27 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1, high = max(piles);
-        while(low <= high){
-            int mid = (low+high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
             int totalH = findTotal(piles, mid);
-            if(totalH <= h) high = mid-1;
-            else low = mid+1;
+            if (totalH <= h)
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         return low;
     }
 
-    int findTotal(int[] nums, int hour){
+    int findTotal(int[] nums, int hour) {
         int total = 0;
-        for(int num:nums)
-            total += Math.ceil((double)num/(double)hour);
+        for (int num : nums)
+            total += Math.ceil((double) num / (double) hour);
         return total;
     }
 
-    int max(int[] nums){
+    int max(int[] nums) {
         int max = Integer.MIN_VALUE;
-        for(int num:nums)
+        for (int num : nums)
             max = Math.max(max, num);
         return max;
     }
