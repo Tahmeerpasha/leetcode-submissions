02dd48bb58a6925f0481c504ff12d2660c307dcc
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-31 20:08:51 +0530

    Time: 12 ms (32.81%) | Memory: 48.1 MB (5.84%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,47 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int low = max(weights), high = sumOfAllWeights(weights);
+
+        // Brute -> Time => O(N * (high-low+1)) && Space => O(1)
         // for(int i=low; i<=high; i++){
         //     if(canShipAllWithCapacity(weights,i,days))return i;
         // }
 
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(canShipAllWithCapacity(weights,mid,days))high = mid-1;
-            else low = mid+1;
+        // Optimal -> Time => O(N * log(high-low+1)) && Space => O(1)
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canShipAllWithCapacity(weights, mid, days))
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         return low;
     }
 
-    int max(int[] weights){
+    int max(int[] weights) {
         int max = Integer.MIN_VALUE;
-        for(int num: weights){
+        for (int num : weights) {
             max = Math.max(max, num);
         }
         return max;
     }
 
-    int sumOfAllWeights(int[] weights){
+    int sumOfAllWeights(int[] weights) {
         int sum = 0;
-        for(int num: weights)
+        for (int num : weights)
             sum += num;
         return sum;
     }
-    boolean canShipAllWithCapacity(int[] weights, int capacity, int days){
-        int sum=0, totalDays=1;
-        for(int num:weights){
+
+    boolean canShipAllWithCapacity(int[] weights, int capacity, int days) {
+        int sum = 0, totalDays = 1;
+        for (int num : weights) {
             sum += num;
-            if(sum > capacity){
+            if (sum > capacity) {
                 totalDays++;
-                sum=num;
+                sum = num;
             }
         }
-        // if(sum > capacity)totalDays++;
         return totalDays <= days;
     }
 }
