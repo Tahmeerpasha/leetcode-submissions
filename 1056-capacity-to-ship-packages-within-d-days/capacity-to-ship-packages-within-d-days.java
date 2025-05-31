1dbe0caa3348a8490ac9b89ec3422b09e1f7bb79
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-05-31 19:51:18 +0530

    Time: 12 ms (32.81%) | Memory: 48.2 MB (5.84%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = max(weights), high = sumOfAllWeights(weights);
+        // for(int i=low; i<=high; i++){
+        //     if(canShipAllWithCapacity(weights,i,days))return i;
+        // }
+
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(canShipAllWithCapacity(weights,mid,days))high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+
+    int max(int[] weights){
+        int max = Integer.MIN_VALUE;
+        for(int num: weights){
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    int sumOfAllWeights(int[] weights){
+        int sum = 0;
+        for(int num: weights)
+            sum += num;
+        return sum;
+    }
+    // boolean canShipAllWithCapacity(int[] weights, int capacity, int reqDays){
+    //     int days = 1; //First day.
+    //     int load = 0;
+    //     int n = weights.length; //size of array.
+    //     for (int i = 0; i < n; i++) {
+    //         if (load + weights[i] > capacity) {
+    //             days += 1; //move to next day
+    //             load = weights[i]; //load the weight.
+    //         } else {
+    //             //load the weight on the same day.
+    //             load += weights[i];
+    //         }
+    //     }
+    //     return days <= reqDays;
+    // }
+    boolean canShipAllWithCapacity(int[] weights, int capacity, int days){
+        int sum=0, totalDays=1;
+        for(int num:weights){
+            sum += num;
+            if(sum > capacity){
+                totalDays++;
+                sum=num;
+            }
+        }
+        if(sum > capacity)totalDays++;
+        return totalDays <= days;
+    }
+}
