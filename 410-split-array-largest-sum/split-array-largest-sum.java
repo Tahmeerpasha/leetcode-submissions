4cd2f6c38f3789d9feabfe510257e04f0173ee3d
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-02 19:46:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (69.26%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,44 +1,52 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        ArrayList<Integer> list = Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.toCollection(ArrayList::new));
-        return findPages(list, nums.length, k);
-    }
-
-    public int findPages(ArrayList<Integer> arr, int n, int m) {
-        if (n < m)
+        if (nums.length < k)
             return -1;
-        int low = arr.stream()
-                .mapToInt(Integer::intValue)
-                .max()
-                .orElse(Integer.MIN_VALUE);
-        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        int low = max(nums);
+        int high = sum(nums);
 
         // for (int i = low; i <= high; i++) {
-        //     if (canAllocateToAll(arr, i) == m)
+        //     if (numOfSubArrays(nums, i) == k)
         //         return i;
         // }
 
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(canAllocateToAll(arr, mid) > m)low = mid+1;
-            else high = mid-1 ;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (numOfSubArrays(nums, mid) > k)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return low;
     }
 
-    int canAllocateToAll(ArrayList<Integer> arr, int size) {
-        int numOfStudents = 1, sum = 0;
+    int max(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    int sum(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    int numOfSubArrays(int[] arr, int size) {
+        int numOfSubArrays = 1, sum = 0;
 
         for (int num : arr) {
             sum += num;
             if (sum > size) {
-                numOfStudents++;
+                numOfSubArrays++;
                 sum = num;
             }
         }
 
-        return numOfStudents;
+        return numOfSubArrays;
     }
 }
