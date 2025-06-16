f82b4782c0bf3e18c8d0b2a95cc5186d1bddc340
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-16 15:01:18 +0530

    Time: 2 ms (74.56%) | Memory: 53.1 MB (49.36%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,33 +1,28 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // Space -> O(n) and Time -> O(1)
-//     Map<Integer, Integer> elementToTimesMap = new HashMap<>();
-//     for (int num : nums) {
-//       if (!elementToTimesMap.containsKey(num)) {
-//         elementToTimesMap.put(num, 1);
-//       } else {
-//         elementToTimesMap.put(num, elementToTimesMap.get(num) + 1);
-//       }
-//     }
-//     for (Map.Entry<Integer, Integer> entry : elementToTimesMap.entrySet()) {
-//       if (entry.getValue() > nums.length / 2) {
-//         return entry.getKey();
-//       }
-//     }
-//     return 0;
-
-        // Space -> O(1) and Time -> O(N)
-
-    int count = 0;
-    int candidate = 0;
-
-    for(int num: nums){
-        if(count == 0)
-            candidate = num;
-
-        if(num == candidate) count++;
-        else count--;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for (int num : nums) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if (entry.getValue() > nums.length / 2) {
+        //         return entry.getKey();
+        //     }
+        // }
+        // return -1;
+        int candidate = 0, count=0;
+        for(int num: nums){
+            if(count==0){
+                candidate = num;
+                count = 1;
+            }
+            else if(candidate == num)count++;
+            else count--;
+        }
+        count = 0;
+        for(int num:nums){
+            if(num == candidate)count++;
+        }
+        return count > nums.length/2 ? candidate : -1;
     }
-    return candidate;
-  }
 }
