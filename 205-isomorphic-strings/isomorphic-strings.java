3ab90cf8e1bf77decf3a5128bb07ede2b4f139ab
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-20 14:58:17 +0530

    Time: 7 ms (85.06%) | Memory: 42.8 MB (15.77%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,25 +3,34 @@ class Solution {
         if (s.length() != t.length())
             return false;
 
-        Map<Character, Character> mapST = new HashMap<>();
-        Map<Character, Character> mapTS = new HashMap<>();
+        // Map<Character, Character> mapST = new HashMap<>();
+        // Map<Character, Character> mapTS = new HashMap<>();
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     char chS = s.charAt(i);
+        //     char chT = t.charAt(i);
+
+        //     if (mapST.containsKey(chS)) {
+        //         if (mapST.get(chS) != chT)
+        //             return false;
+        //     } else {
+        //         if (mapTS.containsKey(chT))
+        //             return false;
+
+        //         mapST.put(chS, chT);
+        //         mapTS.put(chT, chS);
+        //     }
+        // }
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
 
         for (int i = 0; i < s.length(); i++) {
-            char chS = s.charAt(i);
-            char chT = t.charAt(i);
-
-            if (mapST.containsKey(chS)) {
-                if (mapST.get(chS) != chT)
-                    return false;
-            } else {
-                if (mapTS.containsKey(chT))
-                    return false;
-
-                mapST.put(chS, chT);
-                mapTS.put(chT, chS);
-            }
+            if (mapS[s.charAt(i)] != mapT[t.charAt(i)])
+                return false;
+            mapS[s.charAt(i)] = i + 1;
+            mapT[t.charAt(i)] = i + 1;
         }
-
         return true;
     }
 
