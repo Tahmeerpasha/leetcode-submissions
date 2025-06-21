8bfb2167ff10f819f4eef7e79b816d7af69d7be1
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-21 13:13:36 +0530

    Time: 12 ms (85.66%) | Memory: 45.4 MB (80.19%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,92 +1,52 @@
-// class Solution {
-//     // public String frequencySort(String s) {
-//     //     Map<Character, Integer> map = new HashMap<>();
-//     //     for (int i = 0; i < s.length(); i++) {
-//     //         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-//     //     }
-
-//     //     Map<Character, Integer> sortedMap = map.entrySet()
-//     //             .stream()
-//     //             .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue())) // descending
-//     //             .collect(Collectors.toMap(
-//     //                     Map.Entry::getKey,
-//     //                     Map.Entry::getValue,
-//     //                     (e1, e2) -> e1,
-//     //                     LinkedHashMap::new));
-
-//     //     StringBuilder sb = new StringBuilder();
-//     //     for (Map.Entry<Character, Integer> entry : sortedMap.entrySet()) {
-//     //         char ch = entry.getKey();
-//     //         int freq = entry.getValue();
-//     //         sb.append(String.valueOf(ch).repeat(freq));
-//     //     }
-
-//     //     return sb.toString();
-//     // }
-
-//     public String frequencySort(String s) {
-//         Map<Character, Integer> freqMap = new HashMap<>();
-//         for (char c : s.toCharArray())
-//             freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-
-//         List<Character>[] buckets = new ArrayList[s.length() + 1];
-//         for (char c : freqMap.keySet()) {
-//             int freq = freqMap.get(c);
-//             if (buckets[freq] == null)
-//                 buckets[freq] = new ArrayList<>();
-//             buckets[freq].add(c);
-//         }
-
-//         StringBuilder sb = new StringBuilder();
-//         for (int i = buckets.length - 1; i >= 1; i--) {
-//             if (buckets[i] != null) {
-//                 for (char c : buckets[i]) {
-//                     sb.append(String.valueOf(c).repeat(i));
-//                 }
-//             }
-//         }
-
-//         return sb.toString();
-//     }
-
-// }
-
 class Solution {
-    public static String frequencySort(String s) {
+    // public String frequencySort(String s) {
+    //     Map<Character, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    //     }
 
-        int n = s.length();
+    //     Map<Character, Integer> sortedMap = map.entrySet()
+    //             .stream()
+    //             .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue())) // descending
+    //             .collect(Collectors.toMap(
+    //                     Map.Entry::getKey,
+    //                     Map.Entry::getValue,
+    //                     (e1, e2) -> e1,
+    //                     LinkedHashMap::new));
 
-        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+    //     StringBuilder sb = new StringBuilder();
+    //     for (Map.Entry<Character, Integer> entry : sortedMap.entrySet()) {
+    //         char ch = entry.getKey();
+    //         int freq = entry.getValue();
+    //         sb.append(String.valueOf(ch).repeat(freq));
+    //     }
 
-        for (int i = 0; i < n; i++) {
+    //     return sb.toString();
+    // }
 
-            char c = s.charAt(i);
+    public String frequencySort(String s) {
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray())
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
 
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        List<Character>[] buckets = new ArrayList[s.length() + 1];
+        for (char c : freqMap.keySet()) {
+            int freq = freqMap.get(c);
+            if (buckets[freq] == null)
+                buckets[freq] = new ArrayList<>();
+            buckets[freq].add(c);
         }
 
-        String ans = "";
-        while (!hm.isEmpty()) {
-
-            int max = 0;
-            char maxChar = ' ';
-
-            for (char c : hm.keySet()) {
-
-                if (hm.get(c) > max) {
-                    max = hm.get(c);
-                    maxChar = c;
+        StringBuilder sb = new StringBuilder();
+        for (int i = buckets.length - 1; i >= 1; i--) {
+            if (buckets[i] != null) {
+                for (char c : buckets[i]) {
+                    sb.append(String.valueOf(c).repeat(i));
                 }
-
             }
-
-            for (int i = 0; i < max; i++) {
-                ans += maxChar;
-            }
-
-            hm.remove(maxChar);
         }
 
-        return ans;
+        return sb.toString();
     }
+
 }
