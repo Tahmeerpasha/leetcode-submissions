ae658146bc3c5355f1883703ec8396f7dc36c0db
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-25 18:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (90.19%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,32 +10,46 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode currOdd = null, currEven = null;
-        ListNode temp = head, oddList = null, evenList = null;
-        int count = 0;
-        if(head == null || head.next == null)return head;
-        while (temp != null) {
-            if (count % 2 == 1) {
-                if (evenList == null) {
-                    evenList = new ListNode(temp.val, null);
-                    currEven = evenList;
-                } else {
-                    currEven.next = new ListNode(temp.val, null);
-                    currEven = currEven.next;
-                }
-            } else {
-                if (oddList == null) {
-                    oddList = new ListNode(temp.val, null);
-                    currOdd = oddList;
-                } else {
-                    currOdd.next = new ListNode(temp.val, null);
-                    currOdd = currOdd.next;
-                }
-            }
-            temp = temp.next;
-            count++;
+        if (head == null || head.next == null)
+            return head;
+        // Brute => Time -> O(n) && Space -> O(n)
+        // ListNode currOdd = null, currEven = null;
+        // ListNode temp = head, oddList = null, evenList = null;
+        // int count = 0;
+        // while (temp != null) {
+        //     if (count % 2 == 1) {
+        //         if (evenList == null) {
+        //             evenList = new ListNode(temp.val, null);
+        //             currEven = evenList;
+        //         } else {
+        //             currEven.next = new ListNode(temp.val, null);
+        //             currEven = currEven.next;
+        //         }
+        //     } else {
+        //         if (oddList == null) {
+        //             oddList = new ListNode(temp.val, null);
+        //             currOdd = oddList;
+        //         } else {
+        //             currOdd.next = new ListNode(temp.val, null);
+        //             currOdd = currOdd.next;
+        //         }
+        //     }
+        //     temp = temp.next;
+        //     count++;
+        // }
+        // currOdd.next = evenList;
+        // return oddList;
+
+        // Optimal => Time -> O(n) && Space -> O(1)
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            
+            odd = odd.next;
+            even = even.next;
         }
-        currOdd.next = evenList;
-        return oddList;
+        odd.next = evenHead;
+        return head;
     }
 }
