27e46579288c7a51972e25cc08302f54cfd1168e
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-26 21:17:45 +0530

    Time: 1 ms (99.86%) | Memory: 48.5 MB (76.40%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -14,16 +14,69 @@ public class Solution {
         ListNode temp1 = headA;
         ListNode temp2 = headB;
         // Brute => Time -> O(m+n) && Space -> O(m+n)
-        Map<ListNode, Integer> map = new HashMap<>();
-        while (temp1 != null) {
-            map.put(temp1, 1);
-            temp1 = temp1.next;
+        // Map<ListNode, Integer> map = new HashMap<>();
+        // while (temp1 != null) {
+        //     map.put(temp1, 1);
+        //     temp1 = temp1.next;
+        // }
+        // while (temp2 != null) {
+        //     if (map.get(temp2) != null)
+        //         return temp2;
+        //     temp2 = temp2.next;
+        // }
+        // return null;
+
+        // Better => Time -> O(n+2m) && Space -> O(1)
+        /**
+        Time complexity: O(2max(length of list1,length of list2))+O(abs(length of list1-length of list2))+O(min(length of list1,length of list2))
+Reason: Finding the length of both lists takes max(length of list1, length of list2) because it is found simultaneously for both of them. Moving the head pointer ahead by a difference of them. The next one is for searching.
+         */
+        // // Step 1: Calculate lengths
+        // int lenA = getLength(headA);
+        // int lenB = getLength(headB);
+
+        // // Step 2: Align heads
+        // while (lenA > lenB) {
+        //     headA = headA.next;
+        //     lenA--;
+        // }
+
+        // while (lenB > lenA) {
+        //     headB = headB.next;
+        //     lenB--;
+        // }
+
+        // // Step 3: Move both pointers together
+        // while (headA != null && headB != null) {
+        //     if (headA == headB)
+        //         return headA;
+        //     headA = headA.next;
+        //     headB = headB.next;
+        // }
+
+        // return null;
+
+        if(temp1 == null || temp2 == null)return null;
+        while(temp1 != temp2){
+            // temp1 = temp1.next;
+            // temp2 = temp2.next;
+            // if(temp1 == temp2)return temp1;
+            // if(temp1 == null)temp1 = headB;
+            // if(temp2 == null)temp2 = headA;
+
+            // OR
+            temp1 = temp1 == null ? headB : temp1.next;
+            temp2 = temp2 == null ? headA : temp2.next;
         }
-        while (temp2 != null) {
-            if (map.get(temp2) != null)
-                return temp2;
-            temp2 = temp2.next;
+        return temp1;
+    }
+
+    private int getLength(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
         }
-        return null;
+        return length;
     }
 }
