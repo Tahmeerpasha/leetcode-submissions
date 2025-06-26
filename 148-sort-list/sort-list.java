c5a230650e1636f43813f409c06c9b9d2d4d6f9f
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-26 19:44:25 +0530

    Time: 10 ms (45.22%) | Memory: 56.6 MB (67.34%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -12,20 +12,62 @@ class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null)
             return head;
+        // Brute => Time -> O(n) && Space -> O(n)
+        // ArrayList<Integer> list = new ArrayList<>();
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     list.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // Collections.sort(list);
+        // temp = head;
+        // int i = 0;
+        // while (temp != null) {
+        //     temp.val = list.get(i++);
+        //     temp = temp.next;
+        // }
+        // return head;
 
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while (temp != null) {
-            list.add(temp.val);
-            temp = temp.next;
+        // Optimal => Time -> O(n logn) && Space -> O(1)
+        ListNode middleNode = findMiddleOfLL(head);
+        ListNode leftHalf = head;
+        ListNode rightHalf = middleNode.next;
+        middleNode.next = null;
+        ListNode left = sortList(leftHalf);
+        ListNode right = sortList(rightHalf);
+        return merge2LL(left, right);
+    }
+
+    ListNode merge2LL(ListNode left, ListNode right) {
+        ListNode mergedHead = new ListNode(-1);
+        ListNode temp = mergedHead;
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                temp = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                temp = right;
+                right = right.next;
+            }
         }
-        Collections.sort(list);
-        temp = head;
-        int i = 0;
-        while (temp != null) {
-            temp.val = list.get(i++);
-            temp = temp.next;
+        if (left != null)
+            temp.next = left;
+        else
+            temp.next = right;
+        return mergedHead.next;
+    }
+
+    ListNode findMiddleOfLL(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        return head;
+        return slow;
     }
 }
