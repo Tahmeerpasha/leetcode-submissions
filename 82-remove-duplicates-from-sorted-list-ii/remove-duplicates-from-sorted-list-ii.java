ce5225d9bb01e7c653e89c634bef874d84e28414
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-27 15:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (45.70%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -9,22 +9,45 @@
  * }
  */
 class Solution {
+    // public ListNode deleteDuplicates(ListNode head) {
+    //     ListNode prevNode = null, currNode = head;
+    //     while (currNode != null) {
+    //         if (currNode.next != null && currNode.val == currNode.next.val) {
+    //             int val = currNode.val;
+    //             while (currNode != null && currNode.val == val)
+    //                 currNode = currNode.next;
+    //             if (prevNode != null)
+    //                 prevNode.next = currNode;
+    //             else
+    //                 head = currNode;
+    //         } else {
+    //             prevNode = currNode;
+    //             currNode = currNode.next;
+    //         }
+    //     }
+    //     return head;
+    // }
+
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode prevNode = null, currNode = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prevNode = dummy, currNode = head;
+
         while (currNode != null) {
+            // Check if current node is a duplicate
             if (currNode.next != null && currNode.val == currNode.next.val) {
-                int val = currNode.val;
-                while (currNode != null && currNode.val == val)
+                int duplicateVal = currNode.val;
+                // Skip all nodes with this value
+                while (currNode != null && currNode.val == duplicateVal) {
                     currNode = currNode.next;
-                if (prevNode != null)
-                    prevNode.next = currNode;
-                else
-                    head = currNode;
+                }
+                // Connect prevNode to next distinct node
+                prevNode.next = currNode;
             } else {
                 prevNode = currNode;
                 currNode = currNode.next;
             }
         }
-        return head;
+        return dummy.next;
     }
 }
