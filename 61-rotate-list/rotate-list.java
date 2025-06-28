7e8838b56931e0281cc34c663e9e9ac399b32def
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-06-28 14:01:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.26%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -12,33 +12,37 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null)
             return head;
+
+        // Step 1: Find the length and last node
         int len = 1;
         ListNode last = head;
         while (last.next != null) {
             last = last.next;
             len++;
         }
+
+        // Step 2: Reduce k
         k = k % len;
         if (k == 0)
             return head;
 
-        // Better
+        // Brute => Time -> O(k*n) && Space => O(1)
         // return rotateKTimes(head, k);
 
-        // Optimal
+        // Optimal -> Time => O(n) && Space => O(1)
+        // Step 3: Find new tail (len - k)th node
         int newLastCount = len - k;
         ListNode newLastNode = head;
-        ListNode prevLastNode = null;
-        while (newLastCount > 0) {
-            prevLastNode = newLastNode;
+        for (int i = 1; i < newLastCount; i++) {
             newLastNode = newLastNode.next;
-            newLastCount--;
         }
-        last.next = head;
-        head = prevLastNode.next;
-        prevLastNode.next = null;
 
-        return head;
+        // Step 4: Make the list circular and break it
+        ListNode newHead = newLastNode.next;
+        newLastNode.next = null;
+        last.next = head;
+
+        return newHead;
     }
 
     ListNode rotateKTimes(ListNode head, int k) {
