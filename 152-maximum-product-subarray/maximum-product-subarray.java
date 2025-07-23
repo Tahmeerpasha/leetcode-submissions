56b91b52ffe14c30a8de816924355e309ede032f
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-07-23 11:57:37 +0530

    Time: 1 ms (90.46%) | Memory: 45.1 MB (31.93%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -11,13 +11,15 @@ class Solution {
         // }
 
         // Optimal -> Time = O(n) && Space = O(1)
-        int prefix=1, suffix=1;
-        for(int i=0; i<nums.length; i++){
-            if(prefix == 0) prefix = 1;
-            if(suffix == 0) suffix = 1;
+        int prefix = 1, suffix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (prefix == 0)
+                prefix = 1;
+            if (suffix == 0)
+                suffix = 1;
 
             prefix *= nums[i];
-            suffix *= nums[nums.length-i-1];
+            suffix *= nums[nums.length - i - 1];
 
             max = Math.max(max, Math.max(prefix, suffix));
         }
