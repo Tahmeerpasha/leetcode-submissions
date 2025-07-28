f08e3b0a6550305e4261f3bf7c704703bfa339cb
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-07-28 18:06:00 +0530

    Time: 1 ms (99.91%) | Memory: 44.5 MB (81.41%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -7,20 +7,21 @@ class Solution {
     }
 
     void findCS(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> currList) {
-
-        if (target == 0) {
-            result.add(new ArrayList<>(currList));
+        if (index == candidates.length) {
+            if (target == 0)
+                result.add(new ArrayList<>(currList));
             return;
         }
 
-        if (target > 0 && index < candidates.length) {
+        if (candidates[index] <= target) {
             // Pick the number
             currList.add(candidates[index]);
             findCS(candidates, target - candidates[index], index, result, currList);
 
-            // Don't pick the number
+            // Bactrack step - remove the number
             currList.remove(currList.size() - 1);
-            findCS(candidates, target, index + 1, result, currList);
         }
+        // Don't pick the number
+        findCS(candidates, target, index + 1, result, currList);
     }
 }
