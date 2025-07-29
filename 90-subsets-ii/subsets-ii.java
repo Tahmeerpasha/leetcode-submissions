bd2f13e375d94d78786e14923f88e56deb1d102b
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-07-29 12:55:51 +0530

    Time: 1 ms (99.96%) | Memory: 43.8 MB (18.67%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,23 +1,18 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        // Time -> O(2^n * k) && Space -> O(k*x)
+        // Time -> O(2^n * n) && Space -> O(2^n)
         Arrays.sort(nums);
-        result.add(new ArrayList<>());
         findSubSets(nums, 0, result, new ArrayList<>());
         return result;
     }
 
     void findSubSets(int[] nums, int index, List<List<Integer>> result, List<Integer> currList) {
-        if (index == nums.length) {
-            return;
-        }
-
+        result.add(new ArrayList<>(currList));
         for (int i = index; i < nums.length; i++) {
             if (i > index && nums[i] == nums[i - 1])
                 continue;
             currList.add(nums[i]);
-            result.add(new ArrayList<>(currList));
             findSubSets(nums, i + 1, result, currList);
             currList.remove(currList.size() - 1);
         }
