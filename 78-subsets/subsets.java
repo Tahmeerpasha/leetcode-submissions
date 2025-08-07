4c2e8e586c07ecb6755af9b9695d49c40f0dac9d
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-07 19:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.33%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,12 +1,31 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        // Using recursion
         // Time and Space -> O(n * 2^n)
-        generateSubSets(nums, 0, result, new ArrayList<>());
+        // generateSubSetsUsingRecursion(nums, 0, result, new ArrayList<>());
+        // return result;
+        // Using Bit Manipulation
+        // Time and Space -> O(n * 2^n)
+        return generateSubSetsUsingBitM(nums, result);
+    }
+
+    List<List<Integer>> generateSubSetsUsingBitM(int[] nums, List<List<Integer>> result) {
+        int n = nums.length;
+        int subsets = 1 << n;
+
+        for (int i = 0; i < subsets; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) // Check if kth bit is set
+                    list.add(nums[j]);
+            }
+            result.add(list);
+        }
         return result;
     }
 
-    void generateSubSets(int[] nums, int index, List<List<Integer>> result, List<Integer> currList) {
+    void generateSubSetsUsingRecursion(int[] nums, int index, List<List<Integer>> result, List<Integer> currList) {
         // Base case
         if (index == nums.length) {
             result.add(new ArrayList<>(currList)); // Deep copy
@@ -15,12 +34,12 @@ class Solution {
 
         // Left recursive call
         currList.add(nums[index]);
-        generateSubSets(nums, index + 1, result, currList);
+        generateSubSetsUsingRecursion(nums, index + 1, result, currList);
 
         // Backtrack step
         currList.remove(currList.size() - 1);
 
         // Right recursive call
-        generateSubSets(nums, index + 1, result, currList);
+        generateSubSetsUsingRecursion(nums, index + 1, result, currList);
     }
 }
