2211db82b07b87ad18bf7c1f068f19a48c4a0ad4
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-11 19:44:45 +0530

    Time: 1 ms (69.18%) | Memory: 40.6 MB (95.52%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -2,7 +2,8 @@ class Solution {
     public int divide(int dividend, int divisor) {
         if (dividend == divisor)
             return 1;
-        if (divisor == 1)return dividend;
+        if (divisor == 1)
+            return dividend;
 
         // Handle sign
         boolean sign = (dividend >= 0) == (divisor >= 0);
@@ -15,17 +16,17 @@ class Solution {
 
         while (n >= d) { // run this till numerator is greater or equal to denominator
             int cnt = 0;
-            while (n >= (d << (cnt + 1))) {// Checking for one step ahead so we can stop count at the exact step 
+            // Checking for one step ahead so we can stop count at the exact step 
+            while (n >= (d << (cnt + 1)))
                 cnt++;
-            }
             quotient += 1L << cnt;
             n -= d << cnt; // Removing the (3 x 2^cnt) from n
         }
 
         // Handle overflow
-        if (quotient >= (1L << 31)) { // If quotient is greater than 2^31 means overflow
+        // If quotient is greater than 2^31 means overflow
+        if (quotient >= (1L << 31))
             return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
 
         return sign ? (int) quotient : (int) -quotient;
     }
