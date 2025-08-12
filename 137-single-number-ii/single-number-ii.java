d44bd163658073814df1dbaf23ac0c2944dadc52
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-12 11:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (80.15%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -14,12 +14,21 @@ class Solution {
         // }
         // return ans;
 
-        // Optimal
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i += 3) {
-            if (nums[i - 1] != nums[i])
-                return nums[i - 1];
+        // Optimal - Time=>O(nlogn + n/3) && Space => O(1)
+        // Arrays.sort(nums);
+        // for (int i = 1; i < nums.length; i += 3) {
+        //     if (nums[i - 1] != nums[i])
+        //         return nums[i - 1];
+        // }
+        // return nums[nums.length - 1];
+
+        // Optimal 2 (Unintuitive so it's ok if you cannot come up with it in interview)
+        // Time -> O(N) && Space -> O(1)
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ones = (nums[i] ^ ones) & ~twos;
+            twos = (nums[i] ^ twos) & ~ones;
         }
-        return nums[nums.length - 1];
+        return ones;
     }
 }
