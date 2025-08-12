5f88777dc11a7754daf897feb527258ff8b818e5
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-12 10:59:39 +0530

    Time: 4 ms (54.70%) | Memory: 45.3 MB (80.15%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,15 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int bitIndex = 0; bitIndex <= 31; bitIndex++) {
-            int count = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if ((nums[i] & (1 << bitIndex)) != 0)
-                    count++;
-            }
-            if (count % 3 == 1)
-                ans = (ans | (1 << bitIndex));
+        // Brute - Using map
+        // Better 
+        // int ans = 0;
+        // for (int bitIndex = 0; bitIndex <= 31; bitIndex++) {
+        //     int count = 0;
+        //     for (int i = 0; i < nums.length; i++) {
+        //         if ((nums[i] & (1 << bitIndex)) != 0)
+        //             count++;
+        //     }
+        //     if (count % 3 == 1)
+        //         ans = (ans | (1 << bitIndex));
+        // }
+        // return ans;
+
+        // Optimal
+        if (nums.length <= 1)
+            return nums[0];
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i += 3) {
+            if (nums[i - 1] != nums[i])
+                return nums[i - 1];
         }
-        return ans;
+        return nums[nums.length - 1];
     }
 }
