e09ee9da788a4ab37d04ed4bcbc08a17bb75454d
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-15 18:01:46 +0530

    Time: 3 ms (76.74%) | Memory: 50.5 MB (27.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -14,30 +14,36 @@ class Solution {
         // });
 
         // Optimal Approach -> Space = O(1) & Time = O(n)
-        int ele1=Integer.MIN_VALUE, ele2=Integer.MIN_VALUE, count1=0, count2=0, n=nums.length;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, count1 = 0, count2 = 0, n = nums.length;
 
-        for(int i=0; i<n; i++){
-            if(count1==0 && ele2 != nums[i]){
-                count1=1;
-                ele1=nums[i];
-            }else if(count2==0 && ele1 != nums[i]){
-                count2=1;
-                ele2=nums[i];
-            }else if(ele1 == nums[i]) count1++;
-            else if(ele2 == nums[i])count2++;
-            else{
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && ele2 != nums[i]) {
+                count1 = 1;
+                ele1 = nums[i];
+            } else if (count2 == 0 && ele1 != nums[i]) {
+                count2 = 1;
+                ele2 = nums[i];
+            } else if (ele1 == nums[i])
+                count1++;
+            else if (ele2 == nums[i])
+                count2++;
+            else {
                 count1--;
                 count2--;
             }
         }
-        count1=0;
-        count2=0;
-        for(int i=0; i<n; i++){
-            if(nums[i] == ele1) count1++;
-            if(nums[i] == ele2) count2++;
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1)
+                count1++;
+            if (nums[i] == ele2)
+                count2++;
         }
-        if(n/3 < count1) resultantArray.add(ele1);
-        if(n/3 < count2) resultantArray.add(ele2);
+        if (n / 3 < count1)
+            resultantArray.add(ele1);
+        if (n / 3 < count2)
+            resultantArray.add(ele2);
         return resultantArray;
     }
 }
