4ab953c13b7837b2a8064e5a67485424bd016713
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-24 15:45:23 +0530

    Time: 4 ms (11.25%) | Memory: 42 MB (19.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,56 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        // Brute
-        // if (s.length() <= 1)
-        //     return false;
-        // Stack<Character> stack = new Stack<>();
-        // for (int i = 0; i < s.length(); i++) {
-        //     if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
-        //         stack.add(s.charAt(i));
-        //     if (s.charAt(i) == ')') {
-        //         if (stack.size() != 0 && stack.peek() == '(')
-        //             stack.pop();
-        //         else
-        //             return false;
-        //     }
-        //     if (s.charAt(i) == '}') {
-        //         if (stack.size() != 0 && stack.peek() == '{')
-        //             stack.pop();
-        //         else
-        //             return false;
-        //     }
-        //     if (s.charAt(i) == ']') {
-        //         if (stack.size() != 0 && stack.peek() == '[')
-        //             stack.pop();
-        //         else
-        //             return false;
-        //     }
-        // }
-        // return stack.size() == 0;
-
-        // Optimised
-        // Odd length can't be valid
+        // Time -> O(N) && Space -> O(N)
+        // Odd length are not acceptable
         if (s.length() % 2 != 0)
             return false;
-
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-
-        for (char c : s.toCharArray()) {
-            if (map.containsValue(c)) {
-                // opening bracket
-                stack.push(c);
-            } else if (map.containsKey(c)) {
-                // closing bracket
-                if (stack.isEmpty() || stack.pop() != map.get(c)) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+                stack.add(s.charAt(i));
+            else if (stack.isEmpty())
+                return false;
+            else {
+                Character ch = stack.pop();
+                if (s.charAt(i) == ')' && ch == '(' || s.charAt(i) == '}' && ch == '{'
+                        || s.charAt(i) == ']' && ch == '[')
+                    continue;
+                else
                     return false;
-                }
             }
         }
-
         return stack.isEmpty();
     }
 }
