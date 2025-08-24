138b33a6547daec29556519d9fd147d590f4a358
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-24 15:32:41 +0530

    Time: 4 ms (11.25%) | Memory: 41.9 MB (47.77%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,30 +1,56 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() <= 1)
+        // Brute
+        // if (s.length() <= 1)
+        //     return false;
+        // Stack<Character> stack = new Stack<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+        //         stack.add(s.charAt(i));
+        //     if (s.charAt(i) == ')') {
+        //         if (stack.size() != 0 && stack.peek() == '(')
+        //             stack.pop();
+        //         else
+        //             return false;
+        //     }
+        //     if (s.charAt(i) == '}') {
+        //         if (stack.size() != 0 && stack.peek() == '{')
+        //             stack.pop();
+        //         else
+        //             return false;
+        //     }
+        //     if (s.charAt(i) == ']') {
+        //         if (stack.size() != 0 && stack.peek() == '[')
+        //             stack.pop();
+        //         else
+        //             return false;
+        //     }
+        // }
+        // return stack.size() == 0;
+
+        // Optimised
+        // Odd length can't be valid
+        if (s.length() % 2 != 0)
             return false;
+
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
-                stack.add(s.charAt(i));
-            if (s.charAt(i) == ')') {
-                if (stack.size() != 0 && stack.peek() == '(')
-                    stack.pop();
-                else
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                // opening bracket
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                // closing bracket
+                if (stack.isEmpty() || stack.pop() != map.get(c)) {
                     return false;
-            }
-            if (s.charAt(i) == '}') {
-                if (stack.size() != 0 && stack.peek() == '{')
-                    stack.pop();
-                else
-                    return false;
-            }
-            if (s.charAt(i) == ']') {
-                if (stack.size() != 0 && stack.peek() == '[')
-                    stack.pop();
-                else
-                    return false;
+                }
             }
         }
-        return stack.size() == 0;
+
+        return stack.isEmpty();
     }
 }
