bb762df567e20c929cf4dc2d9d15fae4f92e22f0
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-28 17:30:55 +0530

    Time: 28 ms (5.95%) | Memory: 45.3 MB (7.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,21 +1,43 @@
 class Solution {
+    // public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    //     int result[] = new int[nums1.length];
+    //     int k = 0;
+    //     for (int i = 0; i < nums1.length; i++) {
+    //         int num = nums1[i];
+    //         for (int j = 0; j < nums2.length; j++) {
+    //             if (nums2[j] == num) {
+    //                 int grt = -1;
+    //                 for (int l = j + 1; l < nums2.length; l++) {
+    //                     if (l < nums2.length && nums2[l] > num) {
+    //                         grt = nums2[l];
+    //                         break;
+    //                     }
+    //                 }
+    //                 result[k++] = grt;
+    //             }
+    //         }
+    //     }
+    //     return result;
+    // }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int result[] = new int[nums1.length];
-        int k = 0;
-        for (int i = 0; i < nums1.length; i++) {
-            int num = nums1[i];
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == num) {
-                    int grt = -1;
-                    for (int l = j + 1; l < nums2.length; l++) {
-                        if (l < nums2.length && nums2[l] > num) {
-                            grt = nums2[l];
-                            break;
-                        }
-                    }
-                    result[k++] = grt;
-                }
+        int n1 = nums1.length, k = n1 - 1;
+        int n2 = nums2.length;
+        int[] result = new int[n1];
+        for (int i = n1 - 1; i >= 0; i--) {
+            Stack<Integer> st = new Stack<>();
+            for (int j = n2 - 1; j >= 0 && nums1[i] != nums2[j]; j--) {
+                st.push(nums2[j]);
             }
+            if (st.isEmpty() && k >= 0) {
+                result[k--] = -1;
+                continue;
+            }
+            while (!st.isEmpty() && st.peek() < nums1[i])
+                st.pop();
+            if (st.isEmpty())
+                result[k--] = -1;
+            else
+                result[k--] = st.peek();
         }
         return result;
     }
