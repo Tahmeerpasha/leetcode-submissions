026ca734cbcdfa1d394a42d6b56619fb55114cf4
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-08-31 17:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (8.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,11 +1,32 @@
 class Solution {
     public int trap(int[] height) {
-        int[] prefixMax = prefixMax(height);
-        int[] suffixMax = suffixMax(height);
-        int total = 0;
-        for (int i = 0; i < height.length; i++) {
-            if (height[i] < prefixMax[i] && height[i] < suffixMax[i]) {
-                total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+        // Brute -> Time => O(3n) && Space => O(2n) 
+        // int[] prefixMax = prefixMax(height);
+        // int[] suffixMax = suffixMax(height);
+        // int total = 0;
+        // for (int i = 0; i < height.length; i++) {
+        //     if (height[i] < prefixMax[i] && height[i] < suffixMax[i]) {
+        //         total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+        //     }
+        // }
+        // return total;
+
+        // Optimal -> Time => O(n) && Space => O(1)
+        int leftMax = 0, rightMax = 0, total = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (leftMax > height[left])
+                    total += leftMax - height[left];
+                else
+                    leftMax = height[left];
+                left++;
+            } else {
+                if (rightMax > height[right])
+                    total += rightMax - height[right];
+                else
+                    rightMax = height[right];
+                right--;
             }
         }
         return total;
