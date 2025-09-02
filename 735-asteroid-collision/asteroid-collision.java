755e854e0283e2bff1222f83093fa74ccf9eb0d7
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-02 13:18:26 +0530

    Time: 5 ms (84.39%) | Memory: 45.4 MB (54.65%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        // Optimal -> Time => O(2n) && Space => O(n)
         List<Integer> st = new ArrayList<>();
-        for (int i = 0; i < asteroids.length; i++) {
-            if (asteroids[i] > 0)
-                st.add(asteroids[i]);
-            else {
-                while (st.size() != 0 && st.get(st.size() - 1) >= 0
-                        && st.get(st.size() - 1) < Math.abs(asteroids[i]))
-                    st.remove(st.size() - 1);
+        for (int a : asteroids) {
+            if (a > 0) {
+                st.add(a);
+            } else {
+                while (!st.isEmpty() && st.get(st.size() - 1) > 0
+                        && st.get(st.size() - 1) < Math.abs(a)) {
+                    st.remove(st.size() - 1); // smaller right asteroids destroyed
+                }
 
-                if (st.size() != 0 && st.get(st.size() - 1) == Math.abs(asteroids[i]))
-                    st.remove(st.size() - 1);
-                else if (st.size() == 0 || st.get(st.size() - 1) < 0)
-                    st.add(asteroids[i]);
+                if (!st.isEmpty() && st.get(st.size() - 1) == Math.abs(a)) {
+                    st.remove(st.size() - 1); // equal size collision
+                } else if (st.isEmpty() || st.get(st.size() - 1) < 0) {
+                    st.add(a); // no collision
+                }
             }
         }
+
         int[] result = new int[st.size()];
-        for (int i = 0; i < result.length; i++)
+        for (int i = 0; i < st.size(); i++)
             result[i] = st.get(i);
         return result;
     }
