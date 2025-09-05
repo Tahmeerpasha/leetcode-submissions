c650e8fb641d5b5e2e3de3ca4d59cb291323a269
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-05 15:36:53 +0530

    Time: 63 ms (79.34%) | Memory: 57.4 MB (72.65%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,16 +1,43 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] pse = findPSEE(heights);
-        int[] nse = findNSE(heights);
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < heights.length; i++) {
-            int value = heights[i] * (nse[i] - pse[i] - 1);
-            max = Math.max(max, value);
+        // Brute -> Time = O(5N) && Space = O(5N)
+        // Time -> O(2N) && Space -> O(2N)
+        // int[] pse = findPSEE(heights);
+        // // Time -> O(2N) && Space -> O(2N)
+        // int[] nse = findNSE(heights);
+        // int max = Integer.MIN_VALUE;
+        // // Time -> O(N) && Space -> O(N)
+        // for (int i = 0; i < heights.length; i++) {
+        //     int value = heights[i] * (nse[i] - pse[i] - 1);
+        //     max = Math.max(max, value);
+        // }
+        // return max;
+
+        // Optimal -> Time = O(n) && Space -> O(n)
+        int n = heights.length;
+        int maxArea = Integer.MIN_VALUE;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
+                int currEle = st.pop();
+                int pse = st.isEmpty() ? -1 : st.peek();
+                int nse = i;
+                maxArea = Math.max(maxArea, (heights[currEle] * (nse - pse - 1)));
+            }
+            st.push(i);
         }
-        return max;
+
+        while (!st.isEmpty()) {
+            int currEle = st.pop();
+            int pse = st.isEmpty() ? -1 : st.peek();
+            int nse = n;
+            maxArea = Math.max(maxArea, (heights[currEle] * (nse - pse - 1)));
+        }
+        return maxArea;
     }
 
     int[] findNSE(int[] nums) {
+        // Time -> O(2N) && Space -> O(2N)
         Stack<Integer> st = new Stack<>();
         int[] result = new int[nums.length];
         for (int i = nums.length - 1; i >= 0; i--) {
@@ -23,6 +50,7 @@ class Solution {
     }
 
     int[] findPSEE(int[] nums) {
+        // Time -> O(2N) && Space -> O(2N)
         Stack<Integer> st = new Stack<>();
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
