8db3181fa7909faa1d6d8721b347ea74fee36b58
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-15 20:06:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (70.42%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -5,26 +5,54 @@ class MyQueue {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
+    // Approach 1
+    // public void push(int x) {
+    //     while (stack1.size() != 0)
+    //         stack2.push(stack1.pop());
+    //     stack1.push(x);
+    //     while (stack2.size() != 0)
+    //         stack1.push(stack2.pop());
+    // }
 
+    // public int pop() {
+    //     return stack1.pop();
+    // }
+
+    // public int peek() {
+    //     return stack1.peek();
+    // }
+
+    // public boolean empty() {
+    //     return stack1.size() == 0;
+    // }
+
+    // Approach 2
     public void push(int x) {
-        stack2 = new Stack<>();
-        while (!stack1.isEmpty())
-            stack2.push(stack1.pop());
         stack1.push(x);
-        while (!stack2.isEmpty())
-            stack1.push(stack2.pop());
     }
 
     public int pop() {
-        return stack1.pop();
+        if (stack2.size() != 0)
+            return stack2.pop();
+        else {
+            while (stack1.size() != 0)
+                stack2.push(stack1.pop());
+            return stack2.pop();
+        }
     }
 
     public int peek() {
-        return stack1.peek();
+        if (stack2.size() != 0)
+            return stack2.peek();
+        else {
+            while (stack1.size() != 0)
+                stack2.push(stack1.pop());
+            return stack2.peek();
+        }
     }
 
     public boolean empty() {
-        return stack1.isEmpty();
+        return stack1.size() == 0 && stack2.size() == 0;
     }
 }
 
