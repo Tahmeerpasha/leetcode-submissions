cf7dec22fbfb33524a13afff9199d02ac03810ce
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-23 19:24:30 +0530

    Time: 8 ms (76.10%) | Memory: 44.8 MB (72.43%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -17,21 +17,35 @@ class Solution {
         // }
         // return maxLen;
 
-        // Optimal: TC=>O(n) && SC=>O(26)
+        // Better: TC=>O(26*n) && SC=>O(26)
         int l = 0, r = 0, maxLen = 0, maxF = 0;
         int[] hash = new int[26];
         while (r < s.length()) {
             hash[s.charAt(r) - 'A']++;
             maxF = Math.max(maxF, hash[s.charAt(r) - 'A']);
-            int changes = r - l + 1 - maxF;
-            if (changes <= k) {
-                maxLen = Math.max(maxLen, r - l + 1);
-            } else {
+            while (r - l + 1 - maxF > k) {
                 hash[s.charAt(l) - 'A']--;
                 l++;
             }
+            maxLen = Math.max(maxLen, r - l + 1);
             r++;
         }
         return maxLen;
+        // Optimal: TC=>O(n) && SC=>O(26)
+        // int l = 0, r = 0, maxLen = 0, maxF = 0;
+        // int[] hash = new int[26];
+        // while (r < s.length()) {
+        //     hash[s.charAt(r) - 'A']++;
+        //     maxF = Math.max(maxF, hash[s.charAt(r) - 'A']);
+        //     int changes = r - l + 1 - maxF;
+        //     if (changes <= k) {
+        //         maxLen = Math.max(maxLen, r - l + 1);
+        //     } else {
+        //         hash[s.charAt(l) - 'A']--;
+        //         l++;
+        //     }
+        //     r++;
+        // }
+        // return maxLen;
     }
 }
