d568bc191b59ea3ae6e6fd3a806daf09e88714a9
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-23 18:44:00 +0530

    Time: 13 ms (41.58%) | Memory: 44.4 MB (97.19%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        // Brute: TC=> O(n^2) && SC=> O(n)
+        // int maxLen = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     int[] hash = new int[26];
+        //     int maxF = 0;
+        //     for (int j = i; j < s.length(); j++) {
+        //         hash[s.charAt(j) - 'A']++;
+        //         maxF = Math.max(maxF, hash[s.charAt(j) - 'A']);
+        //         int changes = j - i + 1 - maxF;
+        //         if (changes <= k) {
+        //             maxLen = Math.max(maxLen, j - i + 1);
+        //         } else
+        //             break;
+        //     }
+        // }
+        // return maxLen;
+
+        // Optimal: TC=>O(n) && SC=>O(26)
+        int l = 0, r = 0, maxLen = 0, maxF = 0;
+        int[] hash = new int[26];
+        while (r < s.length()) {
+            hash[s.charAt(r) - 'A']++;
+            maxF = Math.max(maxF, hash[s.charAt(r) - 'A']);
+            int changes = r - l + 1 - maxF;
+            if (changes <= k) {
+                maxLen = Math.max(maxLen, r - l + 1);
+            } else {
+                hash[s.charAt(l) - 'A']--;
+                l++;
+            }
+            r++;
+        }
+        return maxLen;
+    }
+}
