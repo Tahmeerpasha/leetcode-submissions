ca1fb92bbc6d7844e78715825cf02e7950119b40
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-23 20:01:12 +0530

    Time: 1 ms (99.98%) | Memory: 47.3 MB (93.07%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,16 +1,39 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int maxSub = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == goal) {
-                    maxSub++;
-                } else if (sum > goal)
-                    break;
+
+        // Brute: TC=> O(n^2) && SC => O(1)
+        // int count = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     int sum = 0;
+        //     for (int j = i; j < nums.length; j++) {
+        //         sum += nums[j];
+        //         if (sum == goal) {
+        //             count++;
+        //         } else if (sum > goal)
+        //             break;
+        //     }
+        // }
+        // return count;
+
+        // Optimal: TC => O(2* 2n) && SC => O(1)
+        return func(nums, goal) - func(nums, goal - 1);
+
+    }
+
+    // TC - O(2N) && SC - O(1)
+    int func(int[] nums, int goal) {
+        if (goal < 0)
+            return 0;
+        int l = 0, r = 0, count = 0, sum = 0;
+        while (r < nums.length) {
+            sum += nums[r];
+            while (sum > goal) {
+                sum -= nums[l];
+                l++;
             }
+            count += r - l + 1;
+            r++;
         }
-        return maxSub;
+        return count;
     }
 }
