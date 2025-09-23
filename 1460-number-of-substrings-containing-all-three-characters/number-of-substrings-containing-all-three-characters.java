8bb880a76b09fc8d41fc5c483e40af18b3811476
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-09-23 21:15:58 +0530

    Time: 15 ms (34.72%) | Memory: 44.3 MB (97.09%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -20,14 +20,30 @@ class Solution {
         // }
         // return count;
 
-        // Optimal: TC => O(N) && SC => O(1)
-        int[] lastSeen = new int[] { -1, -1, -1 };
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            lastSeen[s.charAt(i) - 'a'] = i;
-            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1)
-                count += 1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]);
+        // Better: TC => O(N) && SC => O(1)
+        int l = 0, r = 0, maxCount = 0;
+        int[] count = new int[3]; // counts for 'a', 'b', 'c'
+        while (r < s.length()) {
+            count[s.charAt(r) - 'a']++;
+            // shrink window from left while all characters are present
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                count[s.charAt(l) - 'a']--;
+                l++;
+            }
+            // all substrings ending at 'right' and starting before 'left' are valid
+            maxCount += l;
+            r++;
         }
-        return count;
+        return maxCount;
+
+        // Optimal: TC => O(N) && SC => O(1)
+        // int[] lastSeen = new int[] { -1, -1, -1 };
+        // int count = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     lastSeen[s.charAt(i) - 'a'] = i;
+        //     if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1)
+        //         count += 1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]);
+        // }
+        // return count;
     }
 }
