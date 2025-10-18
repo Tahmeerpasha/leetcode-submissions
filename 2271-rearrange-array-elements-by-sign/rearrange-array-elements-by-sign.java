1d75bb354db771822bed0a814df3b0d02c1eb8f8
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-10-18 09:04:26 +0530

    Time: 3 ms (100.00%) | Memory: 76.2 MB (91.12%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,36 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-    // Time -> O(n) and Space -> O(n)
-    // int n = nums.length;
-    // int[] positive = new int[n/2];
-    // int[] negative = new int[n/2];
-    // int[] result = new int[n];
-    // int j=0,k=0;
-    // for(int i = 0; i < n; i++){
-    //     if(nums[i] >= 0)
-    //         positive[j++] = nums[i];
-    //     else negative[k++] = nums[i];    
-    // }
-    // k=0;
-    // j=0;
-    // for(int i=0; i<n; i++){
-    //     if(i%2 == 0)
-    //         result[i] = positive[j++];
-    //     else 
-    //         result[i] = negative[k++];
-    // }
-    // return result;
-    // }
+        // Time -> O(n) and Space -> O(n)
+        // int n = nums.length;
+        // int[] positive = new int[n/2];
+        // int[] negative = new int[n/2];
+        // int[] result = new int[n];
+        // int j=0,k=0;
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] >= 0)
+        //         positive[j++] = nums[i];
+        //     else negative[k++] = nums[i];    
+        // }
+        // k=0;
+        // j=0;
+        // for(int i=0; i<n; i++){
+        //     if(i%2 == 0)
+        //         result[i] = positive[j++];
+        //     else 
+        //         result[i] = negative[k++];
+        // }
+        // return result;
+        // }
 
         // Time -> O(n) and Space -> O(n)
         int n = nums.length;
         int[] result = new int[n];
-        int pos=0, neg=1;
-        for(int i=0; i<n; i++){
-            if(nums[i] < 0){
+        int pos = 0, neg = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
                 result[neg] = nums[i];
                 neg += 2;
-            }else{
+            } else {
                 result[pos] = nums[i];
                 pos += 2;
             }
