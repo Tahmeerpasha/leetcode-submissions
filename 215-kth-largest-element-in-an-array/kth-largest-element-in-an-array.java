7056a6ee9fc7e1d19fd04936721b8d7d2eaf0345
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-10-24 15:22:56 +0530

    Time: 63 ms (25.95%) | Memory: 61.8 MB (23.22%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -5,22 +5,23 @@ class Solution {
         // return nums[nums.length-k];
 
         // Better: Time => O(n + k log n && Space => O(n)
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int num : nums) {
-            maxHeap.add(num);
-        }
-        for (int i = 0; i < k - 1; i++) {
-            maxHeap.poll();
-        }
-        return maxHeap.peek();
-
-        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         // for (int num : nums) {
-        //     pq.add(num);
-        //     if (pq.size() > k) {
-        //         pq.poll();
-        //     }
+        //     maxHeap.add(num);
         // }
-        // return pq.poll();
+        // for (int i = 0; i < k - 1; i++) {
+        //     maxHeap.poll();
+        // }
+        // return maxHeap.peek();
+
+        // Optimal 1: Time => O(n log k) && Space => O(k)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.add(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.poll();
     }
 }
