6a00f15f93f5cc946db52db1a90193b4f420fc21
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-10-27 19:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (5.17%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low=0, mid=0, high = nums.length-1;
-        while(mid<=high){
-            if(nums[mid] == 0) swap(nums, low++, mid++);
-            else if (nums[mid] == 2) swap(nums, mid, high--);
-            else mid++;
+        // Dutch national flag algorithm
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0)
+                swap(nums, low++, mid++);
+            else if (nums[mid] == 2)
+                swap(nums, mid, high--);
+            else
+                mid++;
         }
     }
 
-    void swap(int[] nums, int fromIndex, int toIndex){
-        if(fromIndex>toIndex) return;
+    void swap(int[] nums, int fromIndex, int toIndex) {
+        if (fromIndex > toIndex)
+            return;
         int temp = nums[fromIndex];
         nums[fromIndex] = nums[toIndex];
         nums[toIndex] = temp;
