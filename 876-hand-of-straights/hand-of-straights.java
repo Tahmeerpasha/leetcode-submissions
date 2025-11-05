f5e6d99046c3dbbe770d86c0c2b25bf366ba4e12
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-11-05 16:37:34 +0530

    Time: 55 ms (46.89%) | Memory: 48.2 MB (5.18%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,25 +1,50 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        // if (hand.length % groupSize != 0)
+        //     return false;
+        // Map<Integer, Integer> freq = new HashMap<>();
+        // for (int num : hand)
+        //     freq.put(num, freq.getOrDefault(num, 0) + 1);
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for (int num : hand)
+        //     pq.add(num);
+
+        // while (!pq.isEmpty()) {
+        //     int top = pq.poll();
+        //     if (freq.get(top) <= 0)
+        //         continue;
+        //     for (int i = 1; i < groupSize; i++) {
+        //         if (freq.get(top + i) == null || freq.get(top + i) <= 0)
+        //             return false;
+        //         else if (freq.get(top + i) > 0)
+        //             freq.put(top + i, freq.getOrDefault(top + i, 0) - 1);
+        //     }
+        //     freq.put(top, freq.getOrDefault(top, 0) - 1);
+        // }
+        // return true;
+
         if (hand.length % groupSize != 0)
             return false;
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : hand)
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int num : hand)
-            pq.add(num);
+        TreeMap<Integer, Integer> freq = new TreeMap<>();
+        for (int x : hand)
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
 
-        while (!pq.isEmpty()) {
-            int top = pq.poll();
-            if(freq.get(top) <=0)continue;
-            for (int i = 1; i < groupSize; i++) {
-                if (freq.get(top + i) == null || freq.get(top + i) <= 0)
+        while (!freq.isEmpty()) {
+            int start = freq.firstKey();
+            int count = freq.get(start);
+
+            // try to take 'count' groups starting at start
+            for (int v = start; v < start + groupSize; v++) {
+                Integer f = freq.get(v);
+                if (f == null || f < count)
                     return false;
-                else if(freq.get(top+i) > 0)
-                    freq.put(top + i, freq.getOrDefault(top + i, 0) - 1);
+                if (f == count)
+                    freq.remove(v);
+                else
+                    freq.put(v, f - count);
             }
-            freq.put(top, freq.getOrDefault(top, 0) - 1);
         }
         return true;
     }
