2e0268223951938c43d99f89aa216840609713ca
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2025-12-11 16:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (39.26%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -17,22 +17,44 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         // Iterative: TC => O(n) && SC => O(n)
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode node = root;
-        while (true) {
-            if (node != null) {
-                stack.push(node);
-                node = node.left;
-            } else {
-                if (stack.isEmpty())
-                    break;
-                node = stack.pop();
-                result.add(node.val);
-                node = node.right;
-            }
-        }
+        // Stack<TreeNode> stack = new Stack<>();
+        // TreeNode node = root;
+        // while (true) {
+        //     if (node != null) {
+        //         stack.push(node);
+        //         node = node.left;
+        //     } else {
+        //         if (stack.isEmpty())
+        //             break;
+        //         node = stack.pop();
+        //         result.add(node.val);
+        //         node = node.right;
+        //     }
+        // }
         // Recursive: TC => O(n) && SC => O(n)
         // inOrder(root, result);
+
+        // Morris traversal for inorder
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                result.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode prev = curr.left;
+                while (prev.right != null && prev.right != curr)
+                    prev = prev.right;
+
+                if (prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } else {
+                    prev.right = null;
+                    result.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
         return result;
     }
 
