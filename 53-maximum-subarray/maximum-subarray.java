a36fe68cd15fe72a3266b5e74470be007237ff24
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-01-08 17:31:45 +0530

    Time: 1 ms (99.70%) | Memory: 77.3 MB (49.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,62 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // ArrayList<Integer> answers = new ArrayList<>();
-        int n = nums.length;
-        if (n <= 1)
-            return nums[0];
-
-        // // O(n^3) -> time and space -> O(n^3)
-        // for(int i=0; i<n ; i++){
-        //   for(int j=i; j<n; j++){
-        //     int sum = 0;
-        //     for(int k=i; k<=j; k++)
-        //       sum += nums[k];
-        //     answers.add(sum);
-        //   }
-        // }
-        // answers.sort(null);
-        // return !answers.isEmpty() ? answers.get(answers.size()-1) : 0;
-
-        // // O(n^2) -> time and space -> O(n^2)
-        // for(int i=0; i<n ; i++){
-        // int sum = 0;
-        //   for(int j=i; j<n; j++){
-        //       sum += nums[j];
-        //     answers.add(sum);
-        //   }
-        // }
-        // answers.sort(null);
-        // return !answers.isEmpty() ? answers.get(answers.size()-1) : 0;
-
-        // O(n^2) -> time and space -> O(1)
-        // int max = Integer.MIN_VALUE;
-        // for(int i=0; i<n ; i++){
-        // int sum = 0;
-        //   for(int j=i; j<n; j++){
-        //       sum += nums[j];
-        //       max = Math.max(max,sum);
-        //   }
-        // }
-        // return max;
-
-        // O(n^2) -> time and space -> O(1)
-        // int max = Integer.MIN_VALUE;
-        // for(int i=0; i<n ; i++){
-        // int sum = 0;
-        //   for(int j=i; j<n; j++){
-        //       sum += nums[j];
-        //       max = Math.max(max,sum);
-        //   }
-        // }
-        // return max;
-
-        // O(n) -> time and space -> O(1)
         int sum = 0, max = Integer.MIN_VALUE;
         for (int num : nums) {
-            sum += num;
-            max = Math.max(max, sum);
             if (sum < 0)
                 sum = 0;
+            sum += num;
+            max = Math.max(max, sum);
         }
         return max;
     }
