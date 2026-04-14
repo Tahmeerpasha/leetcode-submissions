b46fd7742ab864f3dd16afd9a20a798809d4a289
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-14 18:23:51 +0530

    Time: 2 ms (99.89%) | Memory: 52.6 MB (43.83%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,49 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> resultantArray = new ArrayList<>();
-
-        // Time -> O(n) & Space -> O(n)
-        // Map<Integer, Integer> elementCountMap = new HashMap<>();
-        // for(int i = 0; i < nums.length; i++){
-        //     elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i],0)+1);
-        // }
-        // elementCountMap.forEach((key, value) -> {
-        // if(value > nums.length/3){
-        //     resultantArray.add(key);
-        // }
-        // });
-
-        // Optimal Approach -> Space = O(1) & Time = O(n)
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, count1 = 0, count2 = 0, n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            if (count1 == 0 && ele2 != nums[i]) {
-                count1 = 1;
-                ele1 = nums[i];
-            } else if (count2 == 0 && ele1 != nums[i]) {
-                count2 = 1;
-                ele2 = nums[i];
-            } else if (ele1 == nums[i])
-                count1++;
-            else if (ele2 == nums[i])
-                count2++;
+        int cnt1 = 0, cnt2 = 0, cand1 = 0, cand2 = 0;
+        int n = nums.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int num : nums) {
+            if (cnt1 == 0 && cand2 != num) {
+                cand1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0 && cand1 != num) {
+                cand2 = num;
+                cnt2 = 1;
+            } else if (cand1 == num)
+                cnt1++;
+            else if (cand2 == num)
+                cnt2++;
             else {
-                count1--;
-                count2--;
+                cnt1--;
+                cnt2--;
             }
         }
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == ele1)
-                count1++;
-            if (nums[i] == ele2)
-                count2++;
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int num : nums) {
+            if (num == cand1)
+                cnt1++;
+            else if (num == cand2)
+                cnt2++;
         }
-        if (n / 3 < count1)
-            resultantArray.add(ele1);
-        if (n / 3 < count2)
-            resultantArray.add(ele2);
-        return resultantArray;
+        if (n / 3 < cnt1)
+            result.add(cand1);
+        if (n / 3 < cnt2)
+            result.add(cand2);
+        return result;
     }
 }
