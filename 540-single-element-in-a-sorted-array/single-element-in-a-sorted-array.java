489e217f05ac4f4455c40207fe8565bd332ebdbf
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-15 19:45:10 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (25.43%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -11,19 +11,10 @@ class Solution {
             int mid = low + (high - low) / 2;
             if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
                 return nums[mid];
-            if (mid % 2 == 0) {
-                if (nums[mid + 1] == nums[mid])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
-            } else {
-                if (nums[mid - 1] == nums[mid])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
-            }
-            // if(mid % 2 == 0 && nums[mid + 1] == nums[mid] || mid % 2 == 1 && nums[mid - 1] == nums[mid])low = mid+1;
-            // else high = mid-1;
+            if (mid % 2 == 0 && nums[mid + 1] == nums[mid] || mid % 2 == 1 && nums[mid - 1] == nums[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return -1;
     }
