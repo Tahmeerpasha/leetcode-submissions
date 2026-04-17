76481fe93e82d73e457d32952544855e6f885070
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-17 18:16:41 +0530

    Time: 25 ms (7.92%) | Memory: 82.2 MB (96.24%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,53 +1,48 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if ((long) n < (long) m * k)
+        if (bloomDay.length < m * k)
             return -1;
-        int low = min(bloomDay);
-        int high = max(bloomDay);
-
-        // Brute force ->Time => O((high-low+1)*N) && Space -> O(1)
-        // for (int i = low; i <= high; i++) {
-        //     if (isPossible(bloomDay, i, m, k))
-        //         return i;
-        // }
-
-        // Optimal -> Time => O(N * log(high-low+1))
+        int low = findMin(bloomDay), high = findMax(bloomDay);
+        int answer = -1;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (isPossible(bloomDay, mid, m, k))
+            int mid = low + (high - low) / 2;
+            if (isBouquetFormed(bloomDay, mid, m, k)) {
+                answer = mid;
                 high = mid - 1;
-            else
+            } else
                 low = mid + 1;
         }
-        return low;
+        return answer;
     }
 
-    int min(int[] bloomDay) {
+    boolean isBouquetFormed(int[] bloomDay, int day, int m, int k) {
+        int cnt = 0;
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= day) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            if (cnt == k) {
+                cnt = 0;
+                m--;
+            }
+
+        }
+        return m <= 0;
+    }
+
+    int findMin(int[] bloomDay) {
         int min = Integer.MAX_VALUE;
         for (int num : bloomDay)
             min = Math.min(min, num);
         return min;
     }
 
-    int max(int[] bloomDay) {
+    int findMax(int[] bloomDay) {
         int max = Integer.MIN_VALUE;
         for (int num : bloomDay)
             max = Math.max(max, num);
         return max;
-    }
-
-    boolean isPossible(int[] bloomDays, int day, int m, int k) {
-        int noOfBouquets = 0, count = 0;
-        for (int val : bloomDays) {
-            if (val <= day)
-                count++;
-            else {
-                noOfBouquets += count / k;
-                count = 0;
-            }
-        }
-        noOfBouquets += count / k;
-        return noOfBouquets >= m;
     }
 }
