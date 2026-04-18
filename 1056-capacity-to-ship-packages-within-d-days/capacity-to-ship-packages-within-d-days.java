a95e67c5b0fcdca9940a1027fbccf19f4fe549b6
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-18 15:44:08 +0530

    Time: 10 ms (86.36%) | Memory: 50.4 MB (22.99%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,16 +1,9 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = max(weights), high = sumOfAllWeights(weights);
-
-        // Brute -> Time => O(N * (high-low+1)) && Space => O(1)
-        // for(int i=low; i<=high; i++){
-        //     if(canShipAllWithCapacity(weights,i,days))return i;
-        // }
-
-        // Optimal -> Time => O(N * log(high-low+1)) && Space => O(1)
+        int low = findMax(weights), high = findSum(weights);
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (canShipAllWithCapacity(weights, mid, days))
+            int mid = low + (high - low) / 2;
+            if (canShipWithCapacity(weights, days, mid))
                 high = mid - 1;
             else
                 low = mid + 1;
@@ -18,30 +11,29 @@ class Solution {
         return low;
     }
 
-    int max(int[] weights) {
-        int max = Integer.MIN_VALUE;
+    boolean canShipWithCapacity(int[] weights, int days, int capacity) {
+        int cnt = 1, sum = 0;
         for (int num : weights) {
-            max = Math.max(max, num);
+            sum += num;
+            if (sum > capacity) {
+                sum = num;
+                cnt++;
+            }
         }
+        return cnt <= days;
+    }
+
+    int findMax(int[] weights) {
+        int max = Integer.MIN_VALUE;
+        for (int num : weights)
+            max = Math.max(max, num);
         return max;
     }
 
-    int sumOfAllWeights(int[] weights) {
+    int findSum(int[] weights) {
         int sum = 0;
         for (int num : weights)
             sum += num;
         return sum;
-    }
-
-    boolean canShipAllWithCapacity(int[] weights, int capacity, int days) {
-        int sum = 0, totalDays = 1;
-        for (int num : weights) {
-            sum += num;
-            if (sum > capacity) {
-                totalDays++;
-                sum = num;
-            }
-        }
-        return totalDays <= days;
     }
 }
