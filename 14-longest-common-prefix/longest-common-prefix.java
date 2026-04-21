dac10de276a605102b400ccfa761f12095a7d7f1
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-21 15:01:40 +0530

    Time: 1 ms (63.10%) | Memory: 43.3 MB (53.36%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,37 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Brute => Time -> O(n^2) && Space -> O(1)
-        // String s = strs[0];
-        // int totalCnt = 0;
-        // for (int i = 0; i < s.length(); i++) {
-        //     int cnt = 1;
-        //     for (int j = 1; j < strs.length; j++) {
-        //         if (strs[j].length() > i && strs[j].charAt(i) == s.charAt(i))
-        //             cnt++;
-        //         else {
-        //             cnt = -1;
-        //             break;
-        //         }
-        //     }
-        //     if (cnt == -1)
-        //         break;
-        //     if (cnt == strs.length)
-        //         totalCnt++;
-        // }
-
-        // Optimal approach => Time -> O(nlogn) && Space -> O(1)
-        if (strs.length < 2)
+        if (strs.length == 1) {
             return strs[0];
-        Arrays.sort(strs);
-        String str1 = strs[0];
-        String str2 = strs[strs.length - 1];
-        int i = 0;
-        if (str1.equals(str2))
-            return str1;
-        int min = Math.min(str1.length(), str2.length());
-        while (i < min && str1.charAt(i) == str2.charAt(i)) {
-            i++;
         }
-        return str1.substring(0, i);
+        Arrays.sort(strs);
+        int size1 = strs[0].length();
+        int size2 = strs[strs.length - 1].length();
+        int size = Math.min(size1, size2);
+        for (int i = 0; i < size; i++) {
+            if (strs[0].charAt(i) != strs[strs.length - 1].charAt(i)) {
+                return strs[0].substring(0, i);
+            }
+        }
+        return strs[0].substring(0, size);
     }
 }
