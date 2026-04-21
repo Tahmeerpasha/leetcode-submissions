7354569afc39bab4b89a6861db7d7976fe5efbc5
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-21 15:21:01 +0530

    Time: 4 ms (82.39%) | Memory: 44.3 MB (83.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,27 +2,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length())
             return false;
-        // String str1 = SortString(s).trim();
-        // String str2 = SortString(t).trim();
-        // return str1.equals(str2);
-
-        int[] freq = new int[26];
+        int[] arr = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
+            arr[s.charAt(i) - 'a']++;
         }
         for (int i = 0; i < t.length(); i++) {
-            freq[t.charAt(i) - 'a']--;
+            arr[t.charAt(i) - 'a']--;
         }
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0)
                 return false;
         }
         return true;
-    }
-
-    public static String SortString(String str) {
-        char c[] = str.toCharArray();
-        Arrays.sort(c);
-        return new String(c);
     }
 }
