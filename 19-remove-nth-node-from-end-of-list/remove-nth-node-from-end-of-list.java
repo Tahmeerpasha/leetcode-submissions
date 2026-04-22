b6d540b4c168e59a0cf6146ad8487b8ba4af9936
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-22 18:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (23.37%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,44 +10,16 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null)
-            return head;
-        if (head.next == null && n == 1)
+        if (head == null || head.next == null && n == 1)
             return null;
-        // Brute => Time -> O(2n) && Space -> O(1)
-        // int count = 0;
-        // ListNode temp = head;
-        // while (temp != null) {
-        //     count++;
-        //     temp = temp.next;
-        // }
-        // temp = head;
-        // int pos = count - n;
-        // if (pos < 0)
-        //     return head;
-        // if (pos == 0)
-        //     return head.next;
-        // count = 0;
-        // while (temp != null) {
-        //     count++;
-        //     if (count == pos)
-        //         break;
-        //     temp = temp.next;
-        // }
-        // if (temp != null && temp.next != null)
-        //     temp.next = temp.next.next;
-        // return head;
-
-        // Optimal => Time -> O(n) && Space -> O(1)
         ListNode fast = head, slow = head;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
             fast = fast.next;
-        }
         if (fast == null)
             return head.next;
         while (fast.next != null) {
-            fast = fast.next;
             slow = slow.next;
+            fast = fast.next;
         }
         slow.next = slow.next.next;
         return head;
