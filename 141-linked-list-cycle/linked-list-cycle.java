164b35497dd66f059fb23af668bf8c1c17910988
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-22 16:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (18.32%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,38 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // Brute => Time -> O(n) && Space -> O(n)
-        // Node temp = head;
-        // Map<Node, int> nodeMap = new HashMap<>();
-        // while (temp != null) {
-        //     if (nodeMap.containsKey(temp)) {
-        //         return true;
-        //     }
-        //     nodeMap.put(temp, 1);
-        //     temp = temp.next;
-        // }
-        // return false;
-
-        // Better => Time -> O(n) && Space -> O(1)
-        // int min = Integer.MIN_VALUE;
-        // ListNode temp = head;
-        // if (head == null || head.next == null)
-        //     return false;
-        // while (temp != null && temp.val != min) {
-        //     temp.val = min;
-        //     temp = temp.next;
-        // }
-        // if (temp == null || temp.next == null)
-        //     return false;
-        // else
-        //     return true;
-
-        // Optimal => Time -> O(n) && Space -> O(1)
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
-            slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast)
+            slow = slow.next;
+            if (fast == slow)
                 return true;
         }
         return false;
