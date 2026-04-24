da5ce2fc4972a6d04f5df370739310edca9601fb
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-04-24 12:35:06 +0530

    Time: 2 ms (95.14%) | Memory: 64.8 MB (86.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,52 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
         int[] answer = new int[n];
-        // // brute force O(n^2)
-        // for(int i=0; i < nums.length; i++){
-        //     int product = 1;
-        //     for(int j=0; j < nums.length; j++){
-        //         if(j != i){
-        //             product *= nums[j];
-        //         }
-        //     }
-        //     answer[i] = product;
-        // }
-        // return answer;
-
-        // Another approach O(n) would be to multiple the entire array and divide by nums[i] to get answer[i] but it is not permissible according to the question.
-
-        //    Better approach || Space -> O(n) || Time -> O(n)
-        // Need to optimise for O(1) space
-            // int[] prefix = new int[n];
-            // int[] suffix = new int[n];
-            // for(int i = 0 ; i < n ; i++){
-            //     prefix[i] = 1;
-            //     suffix[i] = 1;
-            // }
-            // for (int i = 1; i < n; i++) {
-            //     prefix[i] = prefix[i - 1] * nums[i-1];
-            // }
-            // for (int i = n - 2; i >= 0; i--) {
-            //     suffix[i] = suffix[i + 1] * nums[i+1];
-            // }
-            // for(int i=0; i < n ; i++){
-            //     answer[i] = prefix[i] * suffix[i];
-            // }
-            // return answer;
-
-            // Optimised approach || Space -> O(1) || Time -> O(n)
-            int suffix = 1;
-            for(int i = 0 ; i < n ; i++){
-                answer[i] = 1;
-            }
-            for (int i = 1; i < n; i++) {
-                answer[i] = answer[i - 1] * nums[i-1];
-            }
-            for (int i = n - 2; i >= 0; i--) {
-                suffix *= nums[i+1];
-                answer[i] *= suffix;
-            }
-            return answer;
+        int product = 1;
+        for (int i = 0; i < n; i++) {
+            product *= nums[i];
+            prefix[i] = product;
+        }
+        product = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            product *= nums[i];
+            suffix[i] = product;
+        }
+        answer[0] = suffix[1];
+        answer[n - 1] = prefix[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            answer[i] = suffix[i + 1] * prefix[i - 1];
+        }
+        return answer;
     }
 }
