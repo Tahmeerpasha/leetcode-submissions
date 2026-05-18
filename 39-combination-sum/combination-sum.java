ccddc27ee45a1119a0b16223585912eb5928e1c1
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-05-18 19:34:36 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (10.14%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        // Time -> O(2^t * k) where t is how many times we picked an element
-        findCS(candidates, target, 0, result, new ArrayList<>());
+        findCombinationSum(candidates, target, 0, result, new ArrayList<>());
         return result;
     }
 
-    void findCS(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> currList) {
+    void findCombinationSum(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> curr) {
         if (index == candidates.length) {
-            if (target == 0)
-                result.add(new ArrayList<>(currList));
+            if (target == 0) {
+                result.add(new ArrayList<>(curr));
+            }
             return;
         }
-
         if (candidates[index] <= target) {
-            // Pick the number
-            currList.add(candidates[index]);
-            findCS(candidates, target - candidates[index], index, result, currList);
-
-            // Bactrack step - remove the number
-            currList.remove(currList.size() - 1);
+            curr.add(candidates[index]);
+            findCombinationSum(candidates, target - candidates[index], index, result, curr);
+            curr.remove(curr.size() - 1);
         }
-        // Don't pick the number
-        findCS(candidates, target, index + 1, result, currList);
+        findCombinationSum(candidates, target, index + 1, result, curr);
     }
 }
