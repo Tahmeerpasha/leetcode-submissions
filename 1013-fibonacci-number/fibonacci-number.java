b121340cbe0df99eaf84ab22a499cc8127149add
Tahmeer Pasha <93395497+Tahmeerpasha@users.noreply.github.com>
2026-07-25 14:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,19 @@
 class Solution {
     public int fib(int n) {
+        // DP - Memoization | TC -> O(n) && SC -> O(n) + O(n)
+        if (n <= 1)
+            return n;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return fibSeries(n, dp);
+        // Arrays.fill(dp, -1);
+        // return fibSeries(n, dp);
+
+        // DP - Tabulation | TC -> O(n) && SC -> O(n)
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 
     int fibSeries(int n, int[] dp) {
